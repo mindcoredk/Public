1b8108c732622792d38745b1dc566082ddab5efa
Mattias Melkersen <mm@mindcore.dk>
2020-07-08 13:39:51 +0200

    Office 365 Balloon tips go away

---
+++ RemoveBalloons.ps1
@@ -0,0 +1,22 @@
+﻿function Add-Registry {
+
+Param ([string]$RegistryPath,[string]$name,[String]$Value)
+
+IF(!(Test-Path $registryPath))
+
+  {
+
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null}
+
+ ELSE {
+
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null}
+
+Write-Output "Registry added $($registrypath)\$($name)"
+}
+
+Add-Registry -RegistryPath "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\TeachingCallouts" -name "AutocreateTeachingCallout_MoreLocations" -Value "2"
+Add-Registry -RegistryPath "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\TeachingCallouts" -name "SLRToggleReplaceTeachingCalloutID" -Value "2"
+Add-Registry -RegistryPath "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\TeachingCallouts" -name "UseTighterSpacingTeachingCallout" -Value "2"
+Add-Registry -RegistryPath "HKCU:\SOFTWARE\Microsoft\Office\16.0\Common\General" -name "ShownFileFmtPrompt" -Value "1"
