c6bfc286c748b8c10c50064ed3504678f5f94a56
mindcoredk <66680813+mindcoredk@users.noreply.github.com>
2020-08-18 15:58:34 +0200

    Check Windows is activated
    This script can be used together with Endpoint Analytics

---
+++ Check_Windows_Activation.ps1
@@ -0,0 +1,34 @@
+﻿#=============================================================================================================================
+#
+# Script Name:     Check_Windows_Activation.ps1
+# Description:     Detect Windows activated
+# Notes:           
+#                  
+#
+#=============================================================================================================================
+
+Try
+{
+    $LicenseState = Get-CimInstance -ClassName SoftwareLicensingProduct | where {$_.PartialProductKey -and $_.name -like "Windows*"}
+
+    if ($LicenseState.LicenseStatus -ne 1 -or $LicenseState.LicenseStatus -ne 5)
+    {
+        #Exit 1 for machine not licensed correctly
+        Write-Host "Match"
+        exit 1
+    }
+    else 
+    {
+        #Exit 0 for machine licensed.
+        Write-Host "No_Match"            
+        exit 0
+    }
+
+}
+
+catch
+{
+    $errMsg = $_.Exception.Message
+    return $errMsg
+    exit 1
+}
